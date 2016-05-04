e178dc83594b06fe22295dc313e49d566be7b158
jspc <james@zero-internet.org.uk>
2016-05-04 12:32:51 +0100

    Download images

--- src/index.js
+++ src/index.js
@@ -7,54 +7,55 @@ var async = require("async");
 //exports.handler = main(event, context, callback);
 
 var main = function(event, context, callback){
-    var dir = createDir();
+    var tmpdir = createDir(),
+        posterPath = path.join(tmpdir, 'poster.png'),
+        playerPath = path.join(tmpdir, 'player.png'),
+        smallPlayerPath = path.join(tmpdir, 'small_player.png');
 
-    getImages(dir, event.images);
-    // resizeImages();
-    // burnInLayers();
-    // uploadImages();
+
+    async.waterfall([
+        async.apply(download, event.images.poster, posterPath),
+        async.apply(download, event.images.player, playerPath),
+        async.apply(download, event.images.smallPlayer, smallPlayerPath),
+        function(callback){
+            callback(null, tmpdir);
+        },
+        resizeImages,
+        burnInLayers,
+        uploadImages
+    ]);
 };
 
 var createDir = function(){
     return mktemp.createDirSync('/tmp/XXXXXXX');
 };
 
-var getImages = function(tmpdir, images){
-    // wget $images -O $tmpdir/
-    // Rename images to player/poster/small_player
-    var posterPath = path.join(tmpdir, 'poster.png');
-    var playerPath = path.join(tmpdir, 'player.png');
-    var smallPlayerPath = path.join(tmpdir, 'small_player.png');
-
-    download(images.poster, posterPath);
-    download(images.player, playerPath);
-    download(images.smallPlayer, smallPlayerPath);
-};
-
-var download = function(url, dest, cb) {
+var download = function(url, dest, callback) {
     var file = fs.createWriteStream(dest);
-    var request = http.get(url, function(response) {
-        response.pipe(file);
-        file.on('finish', function() {
-            file.close(cb);
+    http.get(url, function (res) {
+        res.pipe(file, {end: 'false'});
+        res.on('end', function () {
+            file.end();
+            console.log("Downloaded " + url + " to " + dest);
+            callback();
         });
-    }).on('error', function(err){
-        fs.unlink(dest);
-        if (cb) cb(err.message);
     });
-    console.log(dest);
 };
 
-var resizeImages = function(tmpdir){
-    // Resize images based on name, from getImages()
+var resizeImages = function(tmpdir, callback){
+    console.log('Resize');
+    callback(null, tmpdir);
 };
 
-var burnInLayers = function(tmpdir){
+var burnInLayers = function(tmpdir, callback){
     // grab some layers from S3 and apply relevant ones
+    console.log('Burn in layers');
+    callback(null, tmpdir);
 };
 
-var uploadImages = function(tmpdir){
-    // shove images to s3
+var uploadImages = function(tmpdir, callback){
+    console.log('Upload images');
+    callback(null, tmpdir);
 };
 
 var event = {
